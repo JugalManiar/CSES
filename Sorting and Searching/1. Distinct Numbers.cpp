35b0b85580df6f9b4710d277df454c31eabcfda4
Jugal Maniar <maniar.jugal@gmail.com>
2026-02-14 23:52:48 +0530

    Add C++ program to count distinct numbers

---
+++ Sorting and Searching/1. Distinct Numbers.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define NeedSpeed ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+typedef set<ll> sll;
+int main(){ NeedSpeed;
+    ll n; sll s; cin>>n; while(cin>>n) s.insert(n); cout<<s.size();
+}
