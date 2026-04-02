f5986539e32855f2b4f6e8afcd7655a42c968840
Jugal Maniar <maniar.jugal@gmail.com>
2026-04-02 21:45:23 +0530

    Add range XOR queries implementation
    Implement prefix XOR array for range XOR queries.

---
+++ 5. Range Queries/05. Range Xor Queries.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define NeedSpeed ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define FOR(X,Y,Z) for(ll X=Y;X<Z;X++)
+int main(){ NeedSpeed;
+    ll n,q,a,b;
+    cin>>n>>q;
+    ll x[n],pre[n+1];
+    pre[0]=0;
+    FOR(i,0,n){
+        cin>>x[i];
+        pre[i+1]=pre[i]^x[i];
+    } while(cin>>a>>b) cout<<(pre[b]^pre[a-1])<<"\n";
+} // Prefix Xor
