7886a0c31fd4579ffebd4ea5ae66029031e8ba54
Jugal Maniar <maniar.jugal@gmail.com>
2026-04-19 12:20:19 +0530

    Refactor labyrinth algorithm for clarity and efficiency

--- 4. Graph Algorithms/02. Labyrinth.cpp
+++ 4. Graph Algorithms/02. Labyrinth.cpp
@@ -4,53 +4,61 @@ using namespace std;
 #define ll long long
 #define FOR(X,Y,Z) for(ll X=Y;X<Z;X++)
 #define pb push_back
+#define F first
+#define S second
 const ll N=1001, M=10000010;
-ll n,m;
-string s[N];
-bool vis[N][N];
-char par[N][N];
-bool b=0;
-vector<char> v;
-void dfs(ll x, ll y, char c){
-    if(!vis[x][y]){
-    // cout<<x<<" "<<y<<" "<<c<<"\n";
-        vis[x][y]=1;
-        par[x][y]=c;
-        if(s[x][y]=='B'){
-            b=1; v.pb(c);
-        }
-        else if(!b&&s[x][y]!='#'){
-            if(x+1<n&&!b) dfs(x+1,y,'D');
-            if(x>1&&!b) dfs(x-1,y,'U');
-            if(y+1<m&&!b) dfs(x,y+1,'R');
-            if(y>1&&!b) dfs(x,y-1,'L');
-            if(b){ v.pb(c); }
-        }
-    }
-}
+char a[N][N];
+list<pair<ll,ll>>lst;
+pair<ll,ll>p[N][N];
+ll level[N][N];
 int main(){ NeedSpeed;
-    ll x,y,xb,yb; cin>>n>>m;
-    FOR(i,0,n){ cin>>s[i];
-        FOR(j,0,m){ vis[i][j]=0;
-            if(s[i][j]=='A'){ x=i, y=j; }
-            if(s[i][j]=='B'){ xb=i, yb=j; }
+    ll n,m,sx,sy,ex,ey,x,y;
+    cin>>n>>m;
+    FOR(i,0,n) FOR(j,0,m){
+        cin>>a[i][j];
+        level[i][j]=M;
+        if(a[i][j]=='A'){
+            sx=i;
+            sy=j;
+        } else if(a[i][j]=='B'){
+            ex=i;
+            ey=j;
         }
-    } vis[n+1][m+1]=0;
-    dfs(x,y,'a');
-    if(v.size()){
-        cout<<"YES\n"<<v.size()<<"\n";
-        for(auto it:v) cout<<it;
+    } lst.pb({sx,sy});
+    level[sx][sy]=0;
+    while(lst.size()&&level[ex][ey]==M){
+        x=(*lst.begin()).F; y=(*lst.begin()).S;
+        if((a[x+1][y]=='B'||a[x+1][y]=='.')&&level[x+1][y]>level[x][y]+1&&x+1<n){
+            level[x+1][y]=level[x][y]+1;
+            p[x+1][y]={x,y}; lst.pb({x+1,y});
+        } if((a[x][y+1]=='B'||a[x][y+1]=='.')&&level[x][y+1]>level[x][y]+1&&y+1<m){
+            level[x][y+1]=level[x][y]+1;
+            p[x][y+1]={x,y}; lst.pb({x,y+1});
+        } if((a[x-1][y]=='B'||a[x-1][y]=='.')&&level[x-1][y]>level[x][y]+1&&x>0){
+            level[x-1][y]=level[x][y]+1;
+            p[x-1][y]={x,y}; lst.pb({x-1,y});
+        } if((a[x][y-1]=='B'||a[x][y-1]=='.')&&level[x][y-1]>level[x][y]+1&&y>0){
+            level[x][y-1]=level[x][y]+1;
+            p[x][y-1]={x,y}; lst.pb({x,y-1});
+        } lst.pop_front();
+    } if(level[ex][ey]<M){
+        vector<char>s; x=ex; y=ey; char f;
+        FOR(i,0,level[ex][ey]){
+            if(p[x][y].F==x-1&&p[x][y].S==y)
+                f='D';
+            else if(p[x][y].F==x+1&&p[x][y].S==y)
+                f='U';
+            else if(p[x][y].F==x&&p[x][y].S==y-1)
+                f='R';
+            else if(p[x][y].F==x&&p[x][y].S==y+1)
+                f='L';
+            s.pb(f);
+            ll xx=x;
+            x=p[x][y].F;
+            y=p[xx][y].S;
+        } cout<<"YES\n"<<level[ex][ey]<<"\n";
+        FOR(i,0,level[ex][ey])
+            cout<<s[level[ex][ey]-1-i];
+        cout<<"\n";
     } else cout<<"NO\n";
-    cout<<"\n"; if(par[xb][yb]){
-        ll i=xb, j=yb; cout<<i<<" "<<j<<"\n";
-        while(i!=x&&j!=y){
-            char f=par[i][j];
-            cout<<f;
-            if(f=='U') j--;
-            else if(f=='D') j++;
-            else if(f=='R') i--;
-            else if(f=='L') i++;
-            else break;
-        }
-    }
 }
